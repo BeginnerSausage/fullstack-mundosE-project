ef005f6d625607d0a7db3c2ba2d17ac64fdb3a15
BeginnerSausage <gmpereyramarcolongo@gmail.com>
2022-05-30 07:26:15 -0300

    actualizacion de api-url

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,7 +16,7 @@ function PostForm() {
     const handleSubmit = function (event){
       event.preventDefault()
       axios({
-        url: 'https://quiet-spire-49475.herokuapp.com/api/contacto',
+        url: 'https://laravel-form-api.herokuapp.com/api/contacto',
         method: 'POST',
         data: userInput
       }).then( result => {
