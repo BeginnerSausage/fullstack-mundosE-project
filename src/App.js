f1cf175cf9c25895541d75a691ab6bc26b1ded21
BeginnerSausage <gmpereyramarcolongo@gmail.com>
2022-06-08 17:02:06 -0300

    Implementación de navbar-mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar-mobile/navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Products from "./components/Products/Products";
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Logo from "../../assets/img/top-logo.svg";
+import Navbar from '../../components/Navbar-mobile/navbar';
 
 const Header = () => {
   const [navbar, setNavbar] = useState(false)
@@ -14,19 +15,30 @@ const Header = () => {
 
   window.addEventListener('scroll', changeBackground);
 
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    //cuando esta true lo pasa a false y viceversa.
+    setClicked(!clicked)
+  }
+
   return (
     <header id="home">
       <div class="container" className={navbar ? 'container-active' : 'container'}>
         <a className="logo" href="#home"
           ><img src={Logo} alt="logo 25Watts"
         /></a>
-        <nav> 
-          <a href="#hero">Home</a>
-          <a href="#about">About</a>
-          <a href="#products">Products</a>
-          <a href="#services">Services</a>
-          <a href="#contact">Contact</a>
+        <nav className={`links ${clicked ? 'active' : ''}`}> 
+          <a onClick={handleClick} href="#hero">Home</a>
+          <a onClick={handleClick} href="#about">About</a>
+          <a onClick={handleClick} href="#products">Products</a>
+          <a onClick={handleClick} href="#services">Services</a>
+          <a onClick={handleClick} href="#contact">Contact</a>
         </nav>
+        <Navbar clicked={clicked} handleClick={handleClick}/>
+      </div>
+      <div className={`initial ${clicked ? 'active' : ''}`}>
+
       </div>
     </header>
   );
